66ab0acf4fdeb1a0c10bef772da363711ae2ee8c
still273 <f.schoger@googlemail.com>
2025-02-05 14:06:45 +0100

    update: runtime and fork

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 from model import get_y_init_given_threshold,ZeroerModel
+import time
 
 DEL = 1e-300
 
@@ -13,8 +14,9 @@ def get_results(true_labels, predicted_labels):
     return p, r, f1
 
 
-def run_zeroer(similarity_features_df, similarity_features_lr,id_dfs,true_labels,LR_dup_free,LR_identical,run_trans):
+def run_zeroer(similarity_features_df, similarity_features_lr,id_dfs,true_labels,LR_dup_free,LR_identical,run_trans, similarity_features_test, true_labels_test):
     similarity_matrix = similarity_features_df.values
+    similarity_matrix_test = similarity_features_test.values
     y_init = get_y_init_given_threshold(similarity_features_df)
     similarity_matrixs = [similarity_matrix,None,None]
     y_inits = [y_init,None,None]
@@ -28,8 +30,10 @@ def run_zeroer(similarity_features_df, similarity_features_lr,id_dfs,true_labels
     c_bay = 0.1
     model, y_pred = ZeroerModel.run_em(similarity_matrixs, feature_names, y_inits,id_dfs,LR_dup_free,LR_identical, run_trans, y_true=true_labels,
                                        hard=False, c_bay=c_bay)
-    if true_labels is not None:
-        p, r, f1 = get_results(true_labels, np.round(np.clip(y_pred + DEL, 0., 1.)).astype(int))
+    time_m = time.process_time()
+    y_test_pred = model.predict_PM(similarity_matrix_test)
+    if true_labels_test is not None:
+        p, r, f1 = get_results(true_labels_test, y_test_pred)#, np.round(np.clip(y_pred + DEL, 0., 1.)).astype(int))
         print("Results after EM:")
         print("F1: {:0.2f}, Precision: {:0.2f}, Recall: {:0.2f}".format(f1, p, r))
-    return y_pred
+    return y_test_pred, time_m
